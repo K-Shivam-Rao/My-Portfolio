1a20f433346305b41e04ba6a4c595e3be6da8c54
Shivam Rao <87097987+K-Shivam-Rao@users.noreply.github.com>
2022-05-23 09:42:05 +0530

    Update script.js

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -23,8 +23,8 @@ loadSong(songs[songIndex]);
 // Update song details
 function loadSong(song) {
   title.innerText = song;
-  audio.src = `https://raw.githubusercontent.com/K-Shivam-Rao/My-Portfolio/raw/main/MusicPlayer/Music/${song}.mp3`;
-  cover.src = `https://github.com/K-Shivam-Rao/My-Portfolio/raw/main/MusicPlayer/Cover/${song}.jpg`;
+  audio.src = `https://github.com/K-Shivam-Rao/My-Portfolio/blob/main/MusicPlayer/Music/${song}.mp3`;
+  cover.src = `https://github.com/K-Shivam-Rao/My-Portfolio/blob/main/MusicPlayer/Cover/${song}.jpg`;
 }
 
 // Play song
